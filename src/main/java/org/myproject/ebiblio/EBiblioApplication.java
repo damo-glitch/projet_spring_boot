989c5c1329647fa2805a94ae7c3d8154853455a2
Junior Gagne <damoncaggne2002@gmail.com>
2024-04-23 11:51:40 +0200

    création du projet principal

---
+++ src/main/java/org/myproject/ebiblio/EBiblioApplication.java
@@ -0,0 +1,13 @@
+package org.myproject.ebiblio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBiblioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EBiblioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/myproject/ebiblio/EBiblioApplicationTests.java
@@ -0,0 +1,13 @@
+package org.myproject.ebiblio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EBiblioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
